4474411732a10186be90b4f4a0f8f4a16e0730a7
Justin Yun <justinyun@google.com>
2019-12-21 10:25:24 +0900

    violet: Enforce native interfaces of product partition as VNDK
    By setting PRODUCT_PRODUCT_VNDK_VERSION := current, native modules in
    product partition are not allowed to use any libs in system partition
    excepted VNDK.
    Enforcing it for coral devices.
    
    Bug: 145580164
    Test: Build and check boot
    Change-Id: Icb818c3c6e74a1211881671d87d205d949209b92
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- device.mk
+++ device.mk
@@ -505,6 +505,9 @@ $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
+# VNDK
+PRODUCT_PRODUCT_VNDK_VERSION := current
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
