c1c925b68dc8787a7d483425d97e09c8ac08f454
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    violet: Inherit from QTI FCM
    * Radio config anyway registeres 1.1 interface only and qti fcm has only 1.1 include
    
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     
-    vendor/derp/config/device_framework_matrix.xml
+    vendor_framework_compatibility_matrix.xml \
     
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
