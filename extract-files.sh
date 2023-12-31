90be4dad92733e4005e9e9e2bc28271fd6427560
Pranav Vashi <neobuddy89@gmail.com>
2024-01-01 01:09:09 +0530

    violet: Link dolby blobs with v33 libstagefright_foundation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -75,6 +75,11 @@ function blob_fixup() {
     vendor/lib64/mediadrm/libwvdrmengine.so | vendor/lib/mediadrm/libwvdrmengine.so | vendor/lib64/libwvhidl.so)
          "${PATCHELF}"  --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
     ;;
+    vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefright_soft_ac4dec.so | \
+    vendor/lib/libstagefrightdolby.so | vendor/lib64/libstagefright_soft_ddpdec.so | \
+    vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefrightdolby.so)
+        $PATCHELF_TOOL --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+    ;;
     esac
 }
 
