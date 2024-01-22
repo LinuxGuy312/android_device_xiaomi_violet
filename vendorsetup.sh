b7745f5e352bf8bf1e97248e9aee3ee10fb411da
selfmusing <mirandamehek@gmail.com>
2024-01-23 00:11:50 +0530

    violet: Added vendorsetup.sh for derp dependencies

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/crdroidandroid/proprietary_vendor_xiaomi_violet.git -b 14.0 vendor/xiaomi/violet --depth=1
+git clone https://github.com/crdroidandroid/android_kernel_xiaomi_violet -b 14.0 kernel/xiaomi/violet --depth=1
+git clone --depth=1 https://gitlab.com/crdroidandroid/android-vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
+git clone --depth=1 https://gitlab.com/crdroidandroid/prebuilts_clang_host_linux-x86_clang-r510928 prebuilts/clang/host/linux-x86/clang-r510928
+git clone --depth=1 https://gitlab.com/crdroidandroid/android_vendor_MiuiCamera -b 14.0-violet vendor/MiuiCamera
