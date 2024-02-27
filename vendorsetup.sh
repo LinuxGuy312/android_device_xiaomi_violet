479e9fb94a2d0614e87f7e3e16081b3533e1fe9d
selfmusing <mirandamehek@gmail.com>
2024-02-27 19:05:33 +0530

    violet: Update Clang version to 18.0.1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/crdroidandroid/proprietary_vendor_xiaomi_violet.git -b 14.0 vendor/xiaomi/violet --depth=1
-git clone https://github.com/crdroidandroid/android_kernel_xiaomi_violet -b 14.0 kernel/xiaomi/violet --depth=1
-git clone --depth=1 https://gitlab.com/crdroidandroid/android-vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
-git clone --depth=1 https://gitlab.com/crdroidandroid/prebuilts_clang_host_linux-x86_clang-r510928 prebuilts/clang/host/linux-x86/clang-r510928
-git clone --depth=1 https://gitlab.com/crdroidandroid/android_vendor_MiuiCamera -b 14.0-violet vendor/MiuiCamera
+git clone https://github.com/Evolution-X-Devices/vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet --depth=1
+git clone https://github.com/kibria5/kernel_xiaomi_violet.git -b 14 kernel/xiaomi/violet --depth=1
+git clone --depth=1 https://gitlab.com/kibria5/vendor_xiaomi-firmware_violet.git -b 14 vendor/xiaomi-firmware/violet
+git clone --depth=1 https://gitlab.com/kibria5/android_prebuilts_clang_host_linux-x86_clang-r522817 -b 14 prebuilts/clang/host/linux-x86/clang-r522817
+git clone --depth=1 https://gitlab.com/kibria5/android_vendor_MiuiCamera -b 14 vendor/MiuiCamera
