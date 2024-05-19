6574c782455069fc7cad21bc0dd75c154d8493aa
selfmusing <mirandamehek@gmail.com>
2024-05-19 19:18:15 +0530

    violet: Add support for ViPER4Android FX

--- device.mk
+++ device.mk
@@ -458,6 +458,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
 
+# V4A
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,4 @@ git clone https://github.com/kibria5/kernel_xiaomi_violet.git -b 14 kernel/xiaom
 git clone --depth=1 https://gitlab.com/kibria5/vendor_xiaomi-firmware_violet.git -b 14 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://gitlab.com/kibria5/android_prebuilts_clang_host_linux-x86_clang-r522817 -b 14 prebuilts/clang/host/linux-x86/clang-r522817
 git clone --depth=1 https://gitlab.com/kibria5/android_vendor_MiuiCamera -b 14 vendor/MiuiCamera
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
