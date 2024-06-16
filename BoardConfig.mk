ffca88fdcece00f29abcf1e1570c984429fd71a0
Arian <arian.kulmer@web.de>
2024-06-16 16:26:15 +0200

    violet: Inherit from common Xiaomi FCM
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -134,11 +134,11 @@ USE_OPENGL_RENDERER := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    
-    vendor_framework_compatibility_matrix.xml \
-    
+$(DEVICE_PATH)/framework_compatibility_matrix.xml \
+hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+vendor/derp/config/device_framework_matrix.xml
+  
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
