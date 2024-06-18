79601e2b1ba086ddb6ae4e2c434a8e495d8feeb1
selfmusing <mirandamehek@gmail.com>
2024-06-18 17:11:19 +0530

    violet: Update vendor dependency

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,8 @@
-git clone --depth=1 https://github.com/Evolution-X-Devices/vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet
+git clone --depth=1 https://github.com/selfmusing/vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet
 git clone --depth=1 https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
-git clone --depth=1 https://gitlab.com/kibria5/vendor_xiaomi-firmware_violet.git -b 14 vendor/xiaomi-firmware/violet
+git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://gitlab.com/kibria5/android_prebuilts_clang_host_linux-x86_clang-r522817 -b 14 prebuilts/clang/host/linux-x86/clang-r522817
 git clone --depth=1 https://gitlab.com/kibria5/android_vendor_MiuiCamera -b 14 vendor/MiuiCamera
 git clone https://github.com/selfmusing/vendor_ViPER4AndroidRepackaged vendor/ViPER4AndroidFXRepackaged
 git clone https://github.com/selfmusing/vendor_bcr vendor/bcr
+
