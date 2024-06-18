17a0e64695d02f6a5fee385e15ae265ae9654e07
selfmusing <mirandamehek@gmail.com>
2024-06-19 02:10:02 +0530

    violet: Nuke ViPER4AndroidFXRepackaged for Dolby

--- device.mk
+++ device.mk
@@ -483,9 +483,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
 
-# ViPER4AndroidFXRepackaged
-$(call inherit-product, vendor/ViPER4AndroidFXRepackaged/config.mk)
-
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
