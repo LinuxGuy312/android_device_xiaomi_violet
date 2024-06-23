35921fcfc7bb01383df30967cb589cdb18e1b592
selfmusing <mirandamehek@gmail.com>
2024-06-24 00:53:33 +0530

    violet: Use hardware/xiaomi from @DerpFest-Devices

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,4 @@ git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-
 git clone --depth=1 https://github.com/psionicprjkt/android_prebuilts_clang_host_linux-x86_clang-r522817 prebuilts/clang/host/linux-x86/clang-r522817 && git lfs fetch && git lfs install && git lfs checkout
 git clone --depth=1 https://gitlab.com/kibria5/android_vendor_MiuiCamera -b 14 vendor/MiuiCamera
 git clone https://github.com/selfmusing/vendor_bcr vendor/bcr
-
+git clone https://github.com/DerpFest-Devices/hardware_xiaomi -b 14 hardware/xiaomi
