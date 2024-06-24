61f305b1097efb25d8ce864ba55292079438fa6f
selfmusing <mirandamehek@gmail.com>
2024-06-25 03:30:21 +0530

    violet: Use common aidl/light from hardware_xiaomi

--- device.mk
+++ device.mk
@@ -324,7 +324,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.xiaomi_sm6150
+    android.hardware.light-service.xiaomi
 
 # Lineage Health
 PRODUCT_PACKAGES += \
