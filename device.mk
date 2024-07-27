f5884819c42fa5fb973405e018bf4b38380df73d
selfmusing <mirandamehek@gmail.com>
2024-07-28 02:38:52 +0530

    violet: Remove Dexpreopt targets * Already included at vendor/derp

--- device.mk
+++ device.mk
@@ -438,12 +438,6 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
     
-# Dex preopt
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    DerpLauncherQuickStep \
-    SettingsGoogle \
-    SystemUIGoogle
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
