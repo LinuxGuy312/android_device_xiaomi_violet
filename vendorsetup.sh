9ef84d8cea95d53db08ab80c22bf06af932f0cb9
selfmusing <mirandamehek@gmail.com>
2024-09-22 13:24:04 +0530

    violet: Update vendor dependency branch for vic

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone --depth=1 https://github.com/selfmusing/vendor_xiaomi_violet.git -b udc vendor/xiaomi/violet
+git clone --depth=1 https://github.com/selfmusing/vendor_xiaomi_violet.git -b vic vendor/xiaomi/violet
 git clone --depth=1 https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 14 vendor/MiuiCamera
