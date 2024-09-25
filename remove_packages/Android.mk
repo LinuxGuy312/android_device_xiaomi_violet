a59ae4e5fd5d64387703f9817ac4db965385d613
selfmusing <mirandamehek@gmail.com>
2024-09-25 11:42:40 +0000

    violet: Update remove_pkg targets according to vic

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,10 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Chrome-Stub AmbientStreaming AppDirectedSMSService Drive Chrome ConnMO OdadPrebuilt CalculatorGooglePrebuilt DCMO CalendarGooglePrebuilt
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AmbientStreaming AppDirectedSMSService Drive ConnMO OdadPrebuilt CalculatorGoogle DCMO CalendarGoogle
 LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera HealthIntelligenceStubPrebuilt KidsSupervisionStub MaestroPrebuilt Maps MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap Photos PixelSupportPrebuilt PlayAutoInstallConfig SwitchAccessPrebuilt PrebuiltGmail
-LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM talkback TurboPrebuilt Tycho USCCDM VZWAPNLib Flash
+LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap Photos PixelSupportPrebuilt PlayAutoInstallConfig SwitchAccessPrebuilt Gmail2
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM talkback Tycho USCCDM VZWAPNLib Flash
 LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger VoiceAccessPrebuilt OBDM_Permissions obdm_stub DevicePolicyPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
