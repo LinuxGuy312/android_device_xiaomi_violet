dbbc1727167455149d6ea313fd39fb11d3929922
Joker-V2 <nipinna0@gmail.com>
2025-04-15 10:59:28 +0000

    violet: Don't include MiuiCamera for now

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -13,7 +13,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Inherit MiuiCamera
-$(call inherit-product, vendor/MiuiCamera/config.mk)
+# $(call inherit-product, vendor/MiuiCamera/config.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
