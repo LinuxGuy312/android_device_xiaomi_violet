3b8301ac957192d339f1e721909ba1c17b33b73d
Eren <101087324+LinuxGuy312@users.noreply.github.com>
2025-04-18 11:09:45 +0000

    violet: Bringup @ProjectInfinity-X

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,7 @@ USE_OPENGL_RENDERER := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/infinity/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
@@ -186,6 +186,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
---
+++ infinity_violet.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2020 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from violet device
+$(call inherit-product, device/xiaomi/violet/device.mk)
+
+# Inherit some common InfinityX stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
+
+# Infinity-X Flags
+INFINITY_BUILD_TYPE := OFFICIAL
+INFINITY_MAINTAINER := Eren
+TARGET_SUPPORTS_BLUR := false
+TARGET_HAS_UDFPS := false
+
+# GApps
+BUILD_WITH_GAPPS := true
+WITH_GAPPS := true
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_LEGACY_BOOTANIMATION := true
+
+# Disable UDFPS animations ( unsupported )
+EXTRA_UDFPS_ICONS := false
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := infinity_violet
+PRODUCT_DEVICE := violet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 7 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="violet-user 10 QKQ1.190915.002 V12.5.1.0.QFHINXM release-keys" \
+    BuildFingerprint="xiaomi/violet/violet:10/QKQ1.190915.002/V12.5.1.0.QFHINXM:user/release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
