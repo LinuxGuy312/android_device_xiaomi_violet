3a897f8fd71aaff3952e56b4114ee1da92a1447c
Joker-V2 <nipinna0@gmail.com>
2025-04-18 17:03:13 +0000

    violet: Add hardware/xiaomi to namespace imports

--- device.mk
+++ device.mk
@@ -404,6 +404,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
+    hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
 # Telephony
